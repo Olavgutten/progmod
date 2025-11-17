b7a54fe40bd841ccca349ebf4b775023cd51ca84
Olavgutten <ofurnes4@gmail.com>
2025-11-17 11:17:26 +0100

    Nigga

--- brownisk.py
+++ brownisk.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov  3 12:53:51 2025
+Created on Mon Nov 17 10:40:05 2025
 
 @author: ofurn
 """
 
-import numpy as np
-import random
+import random as rd
+import matplotlib.pyplot as plt
 
-runder = 1000000
+y = [0]
+x = [0]
 
-x = np.random.uniform(-1, 1, runder)
-y = np.random.uniform(-1, 1, runder)
+retninger = ["høyre", "venstre", "opp", "ned"]
+antallsteg = 1000
+retning = rd.choice(retninger)
 
-forholdjobb = x**2 + y**2 < 1
-antallstemmer = np.sum(forholdjobb)
-sans = (antallstemmer / runder) * 4
+for i in range(antallsteg):
+    if retning == "høyre":
+        x.append(x[i]+1)
+        y.append(y[i])
+    if retning == "ned":
+        y.append(y[i]-1)
+        x.append(x[i])
+    if retning == "opp":
+        y.append(y[i]+1)
+        x.append(x[i])
+    if retning == "venstre":
+        x.append(x[i]-1)
+        y.append(y[i])
+        
+plt.plot(x,y)
+plt.show()       
 
-print(f"P*4 er lik {sans}")
 
-
+print(retning)
---
+++ stockastisk populasjonsmodell.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 17 11:01:29 2025
+
+@author: ofurn
+"""
+
+import random
+import matplotlib.pyplot as plt
+
+befolkning = 500
+levekår = ["dø", "etterkom", "etter"]
+randomiser = random.choice(levekår)
+
+for i in range(100):
+    if randomiser == "etterkom":
+        befolkning = befolkning + 1
+    if randomiser == "etter":
+        befolkning = befolkning + 1
+    if randomiser == "dø":
+        befolkning = befolkning -1
+        
+print(befolkning)
+x = i
+y = befolkning
+plt.plot(x,y)
+plt.show()
+        
